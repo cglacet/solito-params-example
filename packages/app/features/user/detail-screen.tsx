6c5909c664bfecb68426a73df43d66aa06ec01bf
Christian Glacet <christian.glacet@gmail.com>
2024-02-05 16:54:55 +0100

    Example showing update params issue

--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -1,17 +1,50 @@
 import { createParam } from 'solito'
 import { TextLink } from 'solito/link'
+
+import { Pressable } from 'react-native'
 import { Text } from 'app/design/typography'
 import { View } from 'app/design/view'
+import { useCallback } from 'react'
 
-const { useParam } = createParam<{ id: string }>()
+const TEST_USER_ID = 'cglacet'
+const { useParam } = createParam<{ id: string; 'is-news': string }>()
 
 export function UserDetailScreen() {
-  const [id] = useParam('id')
+  const [userId] = useParam('id')
+  const [isNew, setIsNew] = useParam('is-news')
+
+  const onUserChanged = useCallback(
+    (userId: string) => {
+      if (userId === TEST_USER_ID) {
+        console.log('Is a new User')
+        setIsNew(userId)
+      }
+    },
+    [setIsNew],
+  )
 
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="mb-4 text-center font-bold">{`User ID: ${id}`}</Text>
+      <Text className="mb-4 text-center font-bold">{`User ID: ${userId} ${
+        isNew ? ' (new)' : ''
+      }`}</Text>
       <TextLink href="/">👈 Go Home</TextLink>
+      <ChangeUserButton onUserChanged={onUserChanged} />
     </View>
+  )
+}
+
+function ChangeUserButton(props: { onUserChanged: (userId: string) => void }) {
+  const [_, setUserId] = useParam('id')
+
+  const onPress = useCallback(() => {
+    props.onUserChanged(TEST_USER_ID)
+    setUserId(TEST_USER_ID)
+  }, [props, setUserId])
+
+  return (
+    <Pressable onPress={onPress}>
+      <Text>Change user ID to '{TEST_USER_ID}'</Text>
+    </Pressable>
   )
 }
